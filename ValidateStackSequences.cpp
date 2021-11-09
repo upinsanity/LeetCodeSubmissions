66c4d9ef1fa263cbd5c634f70883067ae0858d8b
upinsanity <s.monidipa@gmail.com>
2021-11-09 22:57:52 +0000

    Leetcode #946

--- ValidateStackSequences.cpp
+++ ValidateStackSequences.cpp
@@ -1,4 +1,5 @@
  /*
+ Leetcode #946
  Given two sequences pushed and popped with distinct values, return true if and only if this could have been the result of a sequence of push and pop operations on an initially empty stack.
  Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
  Output: true
